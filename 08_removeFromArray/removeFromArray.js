0fe5349323c1dd1bca51505bcefdab6b7c72e080
nahwoah <119922465+nahwoah@users.noreply.github.com>
2025-10-07 18:14:57 +0300

    solved 08 js

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(str, ...rem) {
+    str = str.filter(item => !rem.includes(item))
+    return str;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
