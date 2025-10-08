6c70e2e97bea77dd47f66ae3923c3471bef29894
nahwoah <119922465+nahwoah@users.noreply.github.com>
2025-10-08 15:33:30 +0300

    solved 09 js

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    
+    if (num1 < 0 || num2 < 0){
+        return "ERROR";
+    } else if (num1 == NaN || num2 == NaN){
+        return "ERROR";
+    } else if (Number.isInteger(num1) == false || Number.isInteger(num2) == false){
+        return "ERROR";
+    } else if (num1 > num2){
+        for (let i = num2; i <= num1; i++){
+            sum += i;
+        }
+        return sum;
+    } else{
+        for (let i = num1; i <= num2; i++){
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
