86d3434233ca0cb316cedfbcfb1d0397f5265af7
nahwoah <119922465+nahwoah@users.noreply.github.com>
2025-10-08 20:33:50 +0300

    solved 12 js

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+  let sum = 0;
+	for (let i=0; i < num.length; i++){
+    sum += num[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let sum = 1;
+  for (let i=0; i < arr.length; i++){
+    sum *= arr[i];
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(num, exponent) {
+	let result = 1;
+  for (let i=0; i < exponent; i++){
+    result *= num;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+	while(num > 0){
+    result *= num;
+    num--;
+  }
+  return result;
 };
 
 // Do not edit below this line
