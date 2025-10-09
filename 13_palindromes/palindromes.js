3033a5b3cc1e9e9030074cf87bc7ad2e7a07a279
nahwoah <119922465+nahwoah@users.noreply.github.com>
2025-10-09 09:25:56 +0300

    solved 13 js

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let j = -1;
+    str = str.toLowerCase();
+    str = str.replace(/[^0-9a-z]/gi, '');
+    for(let i = 0; i < str.length; i++){
+      if (str[i] !== str.at(j)){
+        return false;
+      }
+      j--;
+    }
+    return true;
 };
 
 // Do not edit below this line
