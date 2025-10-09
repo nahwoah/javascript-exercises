2ef353dec240247998ea2ca76a8ae09fe5980e9d
nahwoah <119922465+nahwoah@users.noreply.github.com>
2025-10-09 17:41:19 +0300

    solved 14 js

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0){
+      return "OOPS";
+    }
+    let fib1 = 0;
+    let fib2 = 1;
+    let result = fib1 + fib2;
+    for (let i = 1; i <= num; ++i){
+      fib1 = fib2;
+      fib2 = result;
+      result = fib1 + fib2;
+    }
+    return fib1;
 };
 
 // Do not edit below this line
